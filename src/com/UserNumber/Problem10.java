f11aecc5417a28e428de608f0599c2b74462f470
Ufyanich <tima.sul.2001@gmail.com>
2023-07-09 01:57:35 +0600

    Finish of Program byu a and b creation and use if GCD calculator

--- src/com/UserNumber/Problem10.java
+++ src/com/UserNumber/Problem10.java
@@ -1,5 +1,7 @@
 package com.UserNumber;
 
+import java.util.Scanner;
+
 public class Problem10 {
     public static int calc_GCD(int a, int b) {
         if (b == 0) {
@@ -9,5 +11,16 @@ public class Problem10 {
         return calc_GCD(b, a % b);
     }
 
-    public static void main(String[] args)
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a, b;
+
+        System.out.println("Write 1st");
+        a = scan.nextInt();
+        System.out.println("Write 2nd");
+        b = scan.nextInt();
+
+        int j = calc_GCD(a, b);
+        System.out.println("GCD equals " + j);
+    }
 }
