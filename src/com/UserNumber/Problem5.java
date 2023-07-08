14a20cfa309222b071524d7859dd8f08e743b4cf
Ufyanich <tima.sul.2001@gmail.com>
2023-07-08 23:19:10 +0600

    Creation of Project and Function for Fibonacci Sequence

--- src/com/UserNumber/Problem5.java
+++ src/com/UserNumber/Problem5.java
@@ -3,7 +3,26 @@ package com.UserNumber;
 import java.util.Scanner;
 
 public class Problem5 {
+    public static int sumFib(int n){
+
+        if (n <= 0){
+            System.out.println("Error");
+        }
+
+        if (n == 1 || n == 2){
+            return 1;
+        }
+
+        return sumFib(n - 1) + sumFib(n - 2);
+    }
+
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
+        System.out.println("Write number");
+        int n = scan.nextInt();
+
+
+        int j = sumFib(n);
+        System.out.println(n + " Fib = " + j);
     }
 }
