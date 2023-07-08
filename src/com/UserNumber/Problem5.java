457db99c72038cca78856b1fbe5b0ca7d482636d
Ufyanich <tima.sul.2001@gmail.com>
2023-07-08 22:08:35 +0600

    Start of 5th task and creation of Scanner

---
+++ src/com/UserNumber/Problem5.java
@@ -0,0 +1,9 @@
+package com.UserNumber;
+
+import java.util.Scanner;
+
+public class Problem5 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+    }
+}
