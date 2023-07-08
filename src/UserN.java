ca36d0d42dfdef3903dca6098f321c96fdd9947d
Ufyanich <tima.sul.2001@gmail.com>
2023-07-08 15:14:34 +0600

    Getting n elements from user

--- src/UserN.java
+++ src/UserN.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
+
 public class UserN {
     public static void main(String[] args) {
-        System.out.printf("Lets Start Our First N!");
+        System.out.println("Write number of elements");
 
-        
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        System.out.print("Проверка " + n);
     }
 }
