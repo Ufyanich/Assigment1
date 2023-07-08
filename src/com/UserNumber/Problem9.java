2d34a392f789924f76ce052ac7b09e8838b6c878
Ufyanich <tima.sul.2001@gmail.com>
2023-07-09 01:34:52 +0600

    Creation of k! function and (k-n)! function

--- src/com/UserNumber/Problem9.java
+++ src/com/UserNumber/Problem9.java
@@ -1,5 +1,7 @@
 package com.UserNumber;
 
+import java.util.Scanner;
+
 public class Problem9 {
     public static int sumN(int n){
         if (n > 0)
@@ -13,9 +15,33 @@ public class Problem9 {
     public static int sumK(int k){
         if (k > 0)
         {
-            return k * sumN(k-1);
+            return k * sumK(k-1);
         }
         else {
             return 1;}
+    }
+
+    public static int sumMinus(int n, int k){
+
+        int i = n-k;
+
+        if (i <= 0)
+        {
+            return 1;
+        }
+
+        return i * sumMinus(n-1, k-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n, k;
+
+        System.out.println("Write n");
+        n = scan.nextInt();
+        System.out.println("Write k");
+        k = scan.nextInt();
+
+
     }
 }
