006e6807e9d88c76350e2d5a2865db9cd4466c5b
Ufyanich <tima.sul.2001@gmail.com>
2023-07-09 01:15:17 +0600

    Use of function in Main and Addition of print code

--- src/com/UserNumber/Problem8.java
+++ src/com/UserNumber/Problem8.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Problem8 {
     public static boolean checkDigit(String s){
-        if(s.isEmpty())
-        {return true;}
+        if (s.isEmpty()) {
+            return true;
+        }
 
-        char j = s.charAt(0);
-        if(!Character.isDigit(j))
-        {
+        char i = s.charAt(0);
+        if (!Character.isDigit(i)) {
             return false;
         }
 
@@ -19,5 +19,14 @@ public class Problem8 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
+        System.out.println("Write a string");
+        String s = scan.nextLine();
+
+        boolean i = checkDigit(s);
+
+        if (i){
+            System.out.println("Yes");
+        }
+        else {System.out.println("No");}
     }
 }
