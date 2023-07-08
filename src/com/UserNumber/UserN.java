3e03f4e7982cbaacdac69314b10e6cee36a517dc
Ufyanich <tima.sul.2001@gmail.com>
2023-07-08 19:54:32 +0600

    Taking an array from user with creation of function getMinN insider class

--- src/com/UserNumber/UserN.java
+++ src/com/UserNumber/UserN.java
@@ -1,13 +1,39 @@
 package com.UserNumber;
 
+
 import java.util.Scanner;
 
-public class UserN {
-    public static void main(String[] args) {
-        System.out.println("Write number of elements");
 
+
+public class UserN {
+    public static int getMinN(int n, int[] numList) {
+
+        int min = numList[0];
+
+        for (int i = 1; i < n; i++) {
+            if (numList[i] < min) {
+                min = numList[i];
+            }
+        }
+
+        return min;
+    }
+
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Write number of elements");
         int n = scan.nextInt();
-        System.out.print("Проверка " + n);
+
+        Integer[] numList = new Integer[n];
+
+        for (int i = 0; i < n; i++){
+            System.out.println("Write an Array element" + i);
+            numList[i] = scan.nextInt();
+        }
+
+        for (int j = 0; j < n; j++){
+            System.out.println("Check " + numList[j]);
+        }
     }
 }
